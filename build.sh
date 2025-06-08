57eb47c694df7db932826bdd6f722ffd4c47d5db
kstppd <kpapadakis@protonmail.com>
2025-06-08 06:55:56 +0300

    update to fasm

--- build.sh
+++ build.sh
@@ -4,16 +4,23 @@ INC_FLAGS="-isystem./raylib/src"
 LD_FLAGS=" -Wl,--gc-sections  -fno-use-cxa-atexit -nostartfiles -no-pie -s -L./raylib/src  -Wl,-rpath=./raylib/src  -lraylib"
 CXX_FLAGS="-s -no-pie  -fno-use-cxa-atexit  -fdata-sections -ffunction-sections -fno-rtti -fno-exceptions   -Wall -Wextra  -std=c++20 -Os"
 
-git clone ${RAYLIB_GIT} --depth=1 -b master
+git clone ${RAYLIB_GIT} --depth=1 -b master 
 cd raylib/src
-make PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=SHARED -j 12
+make PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=SHARED -j 12 
 clear
 cd ${BASE}
 
+if [ ! -f "./fasm/fasm" ]; then
+    wget -q https://flatassembler.net/fasm-1.73.32.tgz
+    tar -xvf fasm-1.73.32.tgz
+    rm fasm-1.73.32.tgz
+fi
+
 EXTRA=" -nostdlib -fopenmp -fno-unroll-loops -Wl,--build-id=none -Wl,-z,norelro -ffast-math -fsingle-precision-constant -fmerge-all-constants -fno-unroll-loops -fno-math-errno -falign-functions=1 -falign-jumps=1 -falign-loops=1 -fno-stack-protector -fomit-frame-pointer -ffunction-sections -fdata-sections -Wl,--gc-sections"
 g++ -c ${EXTRA} main.cpp ${INC_FLAGS} ${CXX_FLAGS}
-nasm -f elf64 start.asm -Ox
+./fasm/fasm start.asm 
 g++  ${EXTRA}   start.o  main.o  ${LD_FLAGS} -lc -lm -lraylib -o demo
 strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag demo
 rm start.o main.o
+clear
 ls -ahl demo
--- start.asm
+++ start.asm
@@ -1,21 +1,20 @@
-; start.asm
-; Stuff with __dso handle figured out from here https://stackoverflow.com/questions/34308720/where-is-dso-handle-defined + some googling around
-; Supossedly this is used to register and use??!?? destructors of global objects such as global vars / static stuff. It seems
-; to be used atexit() literally the function to call thos dtors?? 
-; x86x64 specific 
-BITS 64
-EXTERN _exit
-EXTERN jump_start
-GLOBAL _start
+; Here we provide handles to start and exit as wel
+; as jump_start becasue in our demo we have nuked main()
+; to save space
+format ELF64 ; one machine only
 
-SECTION .data
-    GLOBAL __dso_handle
+section '.data' writeable
+public __dso_handle
 __dso_handle:
     dq 0
 
-SECTION .text
+section '.text' executable
+public _start
+extrn _exit
+extrn jump_start
 
 _start:
-    call jump_start       ; call jump start from demo code
-    mov rdi, 0            ; set rdi to 0 
-    call _exit            ; call libc _exit with 0 exit
+    call jump_start
+    mov rdi, 0
+    call _exit
+
